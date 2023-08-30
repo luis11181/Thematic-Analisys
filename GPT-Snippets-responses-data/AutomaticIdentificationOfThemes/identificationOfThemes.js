81c092e8d1a1e07ddf76346811c0f02a3b7c706f
luis cañas <luis11181@hotmail.com>
2023-08-29 20:35:57 -0500

    IdentifiationOfThemes

---
+++ GPT-Snippets-responses-data/AutomaticIdentificationOfThemes/identificationOfThemes.js
@@ -0,0 +1,28 @@
+import OpenAI from 'openai';
+import fs from 'fs';
+
+const openai = new OpenAI({
+  apiKey: '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', // defaults to process.env["OPENAI_API_KEY"]
+});
+
+async function main() {
+  const content = fs.readFileSync('QuestionIdentificationOfThemes.txt', 'utf-8');
+
+  const params = {
+    messages: [{ role: 'user', content }],
+    model: 'gpt-4-0613',
+    n: 1,
+    temperature: 0,
+    max_tokens: 1000,
+  };
+
+  const completion = await openai.chat.completions.create(params);
+
+  const response = completion.choices[0]?.message?.content;
+  fs.writeFileSync('response.txt', response);
+
+ console.log(response);
+}
+
+
+main();
